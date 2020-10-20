6384e867874eb2f5a70f561c87a3b0642def5566
lordSerega <kapelka001@ya.ru>
2020-10-20 18:39:18 +0300

    layout

---
+++ index.php
@@ -0,0 +1,102 @@
+
+    <!DOCTYPE html>
+    <html lang="ru">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Главная</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12 ">
+                    <nav class="navbar navbar-expand-lg nav-pan">
+                        <a class="navbar-brand" href="#"><img src="img/logo.png" class="log" alt=""></a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto navig">
+                                <li class="nav-item"> <a class="nav-link" href="#">Услуги</a> </li>
+                                <li class="nav-item"> <a class="nav-link" href="#">Портфолио</a> </li>
+                                <li class="nav-item"> <a class="nav-link" href="#">Отзывы</a> </li>
+                                <li class="nav-item"> <a class="nav-link" href="#">Вакансии</a> </li>
+                                <li class="nav-item"> <a class="nav-link" href="#">Контакты</a> </li>
+                            </ul>
+                            <div class="contact">
+                                <p class="text-right tel">8(863)243-15-10</p>
+                                <p class="text-right city">Ростов-на-Дону</p>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid no-padding">
+            <div class="row">
+                <div class="col-xl-12"> <img src="img/banner.png" alt="banner" class="img-responsive" />
+                    <div class="container banner">
+                        <div class="description">
+                            <h1>Рекламно-информационное <br> агентство</h1>
+                            <p>Будем рады, если Вы обратитесь в наше Агентство. Мы готовы предложить Вам передовые решения для продвижения Вашего бизнеса.</p>
+                            <form class="form-inline">
+                                <div class="form-group mb-2">
+                                    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Номер телефона"> </div>
+                                <button type="submit" class="btn btn-primary mb-2">Обратный звонок</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <h1 class="text-center news">Новости</h1></div>
+            </div>
+            <div class="row">
+                <div class="card-deck">
+                    <div class="card card-red">
+                        <div class="card-body">
+                            <p class="card-date">15.10.2020</p>
+                            <h5 class="card-title">Акции на размещение баннерного
+панно 20% до конца июля</h5>
+                            <button type="submit" class="btn btn-details">Подробнее</button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-date">15.10.2020</p>
+                            <h5 class="card-title">Акции на размещение баннерного
+панно 20% до конца июля</h5>
+                            <button type="submit" class="btn btn-details">Подробнее</button>
+                        </div>
+                    </div>
+                    <div class="card card-purp">
+                        <div class="card-body">
+                            <p class="card-date">15.10.2020</p>
+                            <h5 class="card-title">Акции на размещение баннерного
+панно 20% до конца июля</h5>
+                            <button type="submit" class="btn btn-details">Подробнее</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid bdown">
+            <ul class="bmenu text-center">
+                <li>Услуги</li>
+                <li>Портфолио</li>
+                <li>Отзывы</li>
+                <li>Вакансии</li>
+                <li>Контакты</li>
+            </ul>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
